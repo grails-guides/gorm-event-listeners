15c6303e48c0e30d43f57d91a75c0b3b33363c0c
Zachary Klein <zak@silver-chalice.com>
2017-06-08 01:07:48 -0500

    Created guide

---
+++ initial/grails-app/controllers/gorm/event/listeners/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package gorm.event.listeners
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ initial/grails-app/init/gorm/event/listeners/BootStrap.groovy
@@ -0,0 +1,9 @@
+package gorm.event.listeners
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
