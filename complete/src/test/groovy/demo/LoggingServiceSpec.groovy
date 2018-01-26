3af5fec2f3e2eca686cc0baaa414c480cf669135
graemerocher <graeme.rocher@gmail.com>
2018-01-26 16:48:54 +0100

    Fix events testing setup

--- complete/src/test/groovy/demo/LoggingServiceSpec.groovy
+++ complete/src/test/groovy/demo/LoggingServiceSpec.groovy
@@ -1,37 +1,40 @@
 package demo
 
-import grails.events.bus.EventBusBuilder
 import grails.gorm.transactions.Rollback
 import grails.gorm.transactions.Transactional
 import org.grails.orm.hibernate.HibernateDatastore
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.grails.testing.GrailsUnitTest
 import org.springframework.transaction.PlatformTransactionManager
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
-class LoggingServiceSpec extends Specification {
+class LoggingServiceSpec extends Specification implements GrailsUnitTest {
 
     @Shared @AutoCleanup HibernateDatastore hibernateDatastore
     @Shared PlatformTransactionManager transactionManager
 
     void setupSpec() {
-        println "setupSpec..."
-        hibernateDatastore = new HibernateDatastore(Book)
+        hibernateDatastore = applicationContext.getBean(HibernateDatastore)
         transactionManager = hibernateDatastore.getTransactionManager()
     }
+
+    @Override
+    Set<String> getIncludePlugins() {
+        return ["eventBus"] as Set
+    }
+
+
+    @Override
+    Closure doWithSpring() {{->
+        loggingService(LoggingService)
+        datastore(HibernateDatastore, Book)
+    }}
 
     @Rollback
     @Transactional
     def "loggingService.afterSave is called after book is saved"() {
         given:
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext()
-        applicationContext.beanFactory.registerSingleton("eventBus", new EventBusBuilder().build())
-        applicationContext.beanFactory.registerSingleton("datastore", hibernateDatastore)
-        applicationContext.beanFactory.registerSingleton("transactionManager", transactionManager)
-        applicationContext.register(LoggingService)
-
-        applicationContext.refresh()
         LoggingService loggingService = applicationContext.getBean(LoggingService)
 
         when:
