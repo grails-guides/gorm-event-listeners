c28f09b6a557840a0cd9b99e7f0b5285069478e5
Zachary Klein <zak@silver-chalice.com>
2018-01-26 17:06:45 -0600

    Added unit test for loggingService

---
+++ complete/src/test/groovy/demo/LoggingServiceUnitSpec.groovy
@@ -0,0 +1,27 @@
+package demo
+
+import grails.gorm.transactions.Transactional
+import grails.testing.gorm.DataTest
+import grails.testing.services.ServiceUnitTest
+import org.grails.datastore.mapping.engine.event.PostInsertEvent
+import spock.lang.Specification
+
+class LoggingServiceUnitSpec extends Specification implements ServiceUnitTest<LoggingService>, DataTest {
+
+    void setupSpec() {
+        mockDomains Book, Audit
+    }
+
+    @Transactional
+    void "test after save"(){
+        given:
+        Book book = new Book(title: 'abc', author: 'abc', pages: 1).save()
+        PostInsertEvent event = new PostInsertEvent(dataStore, book)
+
+        when:
+        service.afterSave(event)
+
+        then:
+        Audit.count() == 1
+    }
+}
